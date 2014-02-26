b331615b9a142399ccd60ee9b69ee2f28e9e906f
Amy Ho <ah673@cornell.edu>
2014-02-25 22:51:10 -0500

    first test. Design for cards should use Enums

--- src/CardTests.java
+++ src/CardTests.java
@@ -1,4 +1,10 @@
+import org.junit.Test;
+
 
 public class CardTests {
-
+	@Test
+	public void aceCardIsACE(){
+		Card ace = new Card(Card.Face.ACE); 
+		assertEquals(Card.Face.ACE, ace.cardFace); 
+	}
 }
