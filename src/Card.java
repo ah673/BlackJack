a47974803c241e76f608f24e7782e48fa46001a6
Amy Ho <ah673@cornell.edu>
2014-02-25 23:13:36 -0500

    weird spacing with Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,13 @@
 public class Card {
-	public static final int NUM_OF_SUITS = 4; 
+	public static final int NUM_OF_SUITS = 4;
+	public final Face cardFace;
+	
 	public enum Face {
 		ACE, TWO, THREE, FOUR, FIVE, 
 		SIX, SEVEN, EIGHT, NINE, TEN,
 		JACK, QUEEN, KING
 	}
-                         
-    public final Face cardFace;  
-      
+	  
     public Card(Face cardFace) {
         this.cardFace = cardFace;
     }
