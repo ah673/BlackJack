382d8681c94d132174dd6ecd376c1964e6ccf35a
Amy Ho <ah673@cornell.edu>
2014-02-25 22:53:50 -0500

    new cardtest test

--- src/CardTests.java
+++ src/CardTests.java
@@ -1,10 +1,16 @@
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
 
 public class CardTests {
 	@Test
 	public void aceCardIsACE(){
-		Card ace = new Card(Card.Face.ACE); 
-		assertEquals(Card.Face.ACE, ace.cardFace); 
+		Card aceCard = new Card(Card.Face.ACE); 
+		assertEquals(Card.Face.ACE, aceCard.cardFace); 
+	}
+	
+	@Test
+	public void queenCardIsQUEEN(){
+		Card queenCard = new Card(Card.Face.QUEEN); 
+		assertEquals(Card.Face.QUEEN, queenCard.cardFace); 
 	}
 }
