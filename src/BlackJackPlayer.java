ceca0daf7762be48635e0556995e437820d52063
Amy Ho <ah673@cornell.edu>
2014-02-27 17:44:07 -0500

    Classes redesigned from test

--- src/BlackJackPlayer.java
+++ src/BlackJackPlayer.java
@@ -1,7 +1,20 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class BlackJackPlayer {
-	ArrayList<Card> hand = new ArrayList<Card>();
 	
-
+	protected List<Card> primaryHand = new ArrayList<Card>();
+	
+	
+	public void receiveCard(Card... dealtCard) { 
+		for (Card card : dealtCard){
+			primaryHand.add(card); 
+		}
+	}
+	
+	public boolean isHandBust(List<Card> hand){
+		if (BlackJackGame.minValueOf(hand) > BlackJackGame.WIN_VALUE)
+			return true; 
+		return false;
+	}
+	
 }
--- src/BlackJackPlayerTest.java
+++ src/BlackJackPlayerTest.java
@@ -1,18 +1,37 @@
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
-
 import static org.junit.Assert.*; 
-
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+import java.util.*;
 
 public class BlackJackPlayerTest {
 	@Test
-	public void ACECardReceivedByDealer(){ 
-		Dealer dealer = new Dealer();
-		Card aceCard = new Card(Card.Face.ACE);
-		Card[] dealtHand = new Card[]{aceCard};
+	public void ACECardReceivedByPlayer(){ 
+		BlackJackPlayer player = new BlackJackPlayer();
+		BlackJackCard aceCard = new BlackJackCard(Card.Face.ACE);
+		Card[] dealtHand = new BlackJackCard[]{aceCard};
 		
-		dealer.receiveCard(aceCard); 
-		assertThat(dealer.hand, contains(dealtHand)); 
+		player.receiveCard(aceCard); 
+		
+		assertThat(player.primaryHand, contains(dealtHand)); 
 	}
+	
+	@Test
+	public void ACETenHasMinValueOf11(){
+		List<Card> hand = new ArrayList<Card>();
+		hand.add(new BlackJackCard(Card.Face.ACE));
+		hand.add(new BlackJackCard(Card.Face.TEN));
+		
+		int valueOfHand = BlackJackGame.minValueOf(hand);
+		int actualMinValueOfHand = 11; 
+		
+		InputReceiver io = new InputReceiver(); 
+		io.sayGreeting(); 
+		
+		assertThat(valueOfHand, is(actualMinValueOfHand));
+
+	}
+	
 
 }
