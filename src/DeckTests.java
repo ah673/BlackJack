22bbf527745af43554a98acd68701c8306c6a795
Amy Ho <ah673@cornell.edu>
2014-02-26 22:25:36 -0500

    first Deck test case -- created deck has 52 cards

--- src/DeckTests.java
+++ src/DeckTests.java
@@ -1,10 +1,14 @@
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import org.junit.Test;
 
 public class DeckTests {
+	private static final int TOTAL_CARDS_PER_DECK = Card.NUM_OF_FACES * Card.NUM_OF_SUITS; 
 	
 	@Test
-	public void test(){
-		
+	public void startingDeckHas52Cards(){
+		Deck deck = new Deck(); 
+		assertThat(deck.deck, hasSize(TOTAL_CARDS_PER_DECK)); 
 	}
-
 }
