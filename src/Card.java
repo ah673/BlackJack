21d1a9a346170b997d461a0c7614e47979b3e51e
Amy Ho <ah673@cornell.edu>
2014-02-27 17:44:47 -0500

    Updates to classes and tests

--- src/Card.java
+++ src/Card.java
@@ -1,16 +1,19 @@
-public class Card {
+public abstract class Card {
 	public static final int NUM_OF_SUITS = 4;
-	public final Face cardFace;
+	public static final int NUM_OF_FACES = 13;
+	
+	protected Face cardFace; 
 	
 	public enum Face {
 		ACE, TWO, THREE, FOUR, FIVE, 
 		SIX, SEVEN, EIGHT, NINE, TEN,
-		JACK, QUEEN, KING
+		JACK, QUEEN, KING; 
 	}
 	
-	public Card(Face cardFace) {
-		this.cardFace = cardFace;
+	public Card (Face cardFace){
+		this.cardFace = cardFace; 
 	}
-        
-
+	
+	abstract int value();
+	
 }
--- src/CardTests.java
+++ src/CardTests.java
@@ -1,16 +1,26 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.hamcrest.core.Is.*;
 
 public class CardTests {
 	@Test
 	public void aceCardIsACE(){
-		Card aceCard = new Card(Card.Face.ACE); 
+		Card aceCard = new BlackJackCard(Card.Face.ACE); 
 		assertEquals(Card.Face.ACE, aceCard.cardFace); 
 	}
 	
 	@Test
 	public void queenCardIsQUEEN(){
-		Card queenCard = new Card(Card.Face.QUEEN); 
+		Card queenCard = new BlackJackCard(Card.Face.QUEEN); 
 		assertEquals(Card.Face.QUEEN, queenCard.cardFace); 
+	}
+	
+	@Test
+	public void queenCardValuedAt10(){
+		BlackJackCard queenCard = new BlackJackCard(Card.Face.QUEEN); 
+		int cardFaceValue = queenCard.value();
+		int actualFaceValue = 10;
+		
+		assertEquals(cardFaceValue, actualFaceValue); 
 	}
 }
