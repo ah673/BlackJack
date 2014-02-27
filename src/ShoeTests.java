37dfc6b73a354cec6ad8eb3a764a351eaea3684b
Amy Ho <ah673@cornell.edu>
2014-02-27 00:34:02 -0500

    added new test -- deck should contain one less card after dealing a card

---
+++ src/ShoeTests.java
@@ -0,0 +1,24 @@
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+
+public class ShoeTests {
+	private static final int TOTAL_CARDS_PER_DECK = Card.NUM_OF_FACES * Card.NUM_OF_SUITS; 
+	
+	@Test
+	public void startingDeckHas52Cards(){
+		Shoe shoe = new Shoe(); 
+		assertThat(shoe.cards, hasSize(TOTAL_CARDS_PER_DECK)); 
+	}
+	
+	@Test
+	public void deckContainsOneLessCardAfterDeal(){
+		Shoe shoe = new Shoe();
+		int sizeBefore = shoe.cardsLeft(); 
+		shoe.dealCard();
+		int sizeAfter = shoe.cardsLeft(); 
+		assertThat(sizeAfter, is(sizeBefore - 1)); 
+	}
+}
