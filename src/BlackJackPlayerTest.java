90a4cb35a5ce532d1c0002069943831369b96c3b
Amy Ho <ah673@cornell.edu>
2014-02-27 21:06:23 -0500

    moved tests to where it belongs

--- src/BlackJackPlayerTest.java
+++ src/BlackJackPlayerTest.java
@@ -18,20 +18,7 @@ public class BlackJackPlayerTest {
 	}
 	
 	@Test
-	public void ACETenHasMinValueOf11(){
-		List<Card> hand = new ArrayList<Card>();
-		hand.add(new BlackJackCard(Card.Face.ACE));
-		hand.add(new BlackJackCard(Card.Face.TEN));
-		
-		int valueOfHand = BlackJackGame.minValueOf(hand);
-		int actualMinValueOfHand = 11; 
-		
-		InputReceiver io = new InputReceiver(); 
-		io.sayGreeting(); 
-		
-		assertThat(valueOfHand, is(actualMinValueOfHand));
-
-	}
+	public void addingValue
 	
 
 }
--- src/GameTest.java
+++ src/GameTest.java
@@ -1,10 +1,28 @@
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 import java.util.*;
+
 import org.junit.Test;
 
 
 public class GameTest {
+	@Test
+	public void ACETenHasMinValueOf11(){
+		List<Card> hand = new ArrayList<Card>();
+		hand.add(new BlackJackCard(Card.Face.ACE));
+		hand.add(new BlackJackCard(Card.Face.TEN));
+		
+		int valueOfHand = BlackJackGame.minValueOf(hand);
+		int actualMinValueOfHand = 11; 
+		
+		InputReceiver io = new InputReceiver(); 
+		io.sayGreeting(); 
+		
+		assertThat(valueOfHand, is(actualMinValueOfHand));
+
+	}
+	
 	@Test
 	public void AceTenHand_NotBust(){
 		List<Card> aceTenHand = new ArrayList<Card>();
